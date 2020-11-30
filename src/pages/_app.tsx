e895c26940acbfa29170e08baba51a9adcaf7f8a
pablo <pblolara21@gmail.com>
2020-11-30 18:51:22 -0300

    Add urql client y conectando con graphql, register mut

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider, createClient } from "urql";
+import theme from "../theme";
+import { AppProps } from "next/app";
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
+const client = createClient({
+  // inicializando client urql on our server
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    // send a cookie to be used when we register, or when we login
+    credentials: "include",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { Form, Formik } from "formik";
 import Wrapper from "../components/Wrapper";
-import InputField from "../components/inputField";
+import InputField from "../components/InputField";
 import { Box, Button } from "@chakra-ui/react";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUTATION = `
+mutation Register($username: String!, $password: String!) {
+  register(options: {username:$username, password:$password}) {
+    user{
+      id
+      username
+      createdAt
+    }
+    errors{
+      field
+      message
+    }
+  }
+}
+`;
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUTATION);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(valores) => {
-          console.log(valores);
+          return register(valores);
         }}
       >
         {({ isSubmitting }) => (
