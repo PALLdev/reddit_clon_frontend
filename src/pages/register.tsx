26656641b016226a784525d8c9e092b1ccd35a70
pablo <pblolara21@gmail.com>
2020-12-01 22:24:44 -0300

    register: handling errors (funcionando)

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,17 +4,31 @@ import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { Box, Button } from "@chakra-ui/react";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/dist/client/router";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
   const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (valores) => {
+        onSubmit={async (valores, { setErrors }) => {
           const response = await register(valores); // response me permite acceder a todos los datos de mi register mutation
+          if (response.data?.register.errors) {
+            // handling errors (Optional chain(?): retorna undefined si no existe data)
+            // [{field: "username", message: "existe un error"}] // asi vienen mis errores desde graphql (array de objetos)
+            // setErrors({  // asi maneja los errores formik (un objeto)
+            //   username: "Hay un error en el username"
+            // })
+            setErrors(toErrorMap(response.data.register.errors)); // util que transforma array de errores en un objeto
+          } else if (response.data?.register.user) {
+            // onsubmit worked, redirecciono al home
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
