93879f05d0224d8bf6f178048b78031c401b965e
pablo <pblolara21@gmail.com>
2020-12-03 23:30:23 -0300

    estado del user &  navbar items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,36 @@
 import { Box, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" padding={4}>
-      <Box ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+  // data is loading
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    // user is not logged in
+    body = (
+      <>
         <NextLink href={"/login"}>
           <Link mr={4}>Login</Link>
         </NextLink>
         <NextLink href={"/register"}>
           <Link>Registro</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    // user is logged in
+    body = <Box>{data.me.username}</Box>;
+  }
+
+  return (
+    <Flex bg="tomato" padding={4}>
+      <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
 };
